44c165e054da68f19f46956f23312fb75c38da6c
Soham Bhattacharjee <soham.onlyn@gmail.com>
2021-11-06 22:37:27 -0700

    null check

--- src/Card.js
+++ src/Card.js
@@ -73,15 +73,18 @@ export default function EmployeeCard(props) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography variant="body2">Current Salary: {currencyFormat.format(employee.currentSalary)}</Typography>
-          <Typography variant="body2">
-            Previous Salaries:
-            {employee.salaries.map((sal => {
-              return <Typography  sx={{ fontSize: 12 }} color="text.secondary">
-                {currencyFormat.format(sal.salary)}&nbsp;
-                {sal.fromDate} - {sal.toDate === FUTURE_DATE ? 'Current' : sal.toDate}
-              </Typography>
-            }))}
-          </Typography>
+          {
+            employee.salaries && employee.salaries.length > 0 && 
+            <Typography variant="body2">
+              Previous Salaries:
+              {employee.salaries.map((sal => {
+                return <Typography sx={{ fontSize: 12 }} color="text.secondary">
+                  {currencyFormat.format(sal.salary)}&nbsp;
+                  {sal.fromDate} - {sal.toDate === FUTURE_DATE ? 'Current' : sal.toDate}
+                </Typography>
+              }))}
+            </Typography>
+          }
         </CardContent>
       </Collapse>
     </Card>
