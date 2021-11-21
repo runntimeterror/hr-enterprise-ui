5ebe02e14de21d4e752d25c6b83487ff1e02786e
Rajat Banerjee <rajatmig29@gmail.com>
2021-11-20 21:22:11 -0800

    upgrading amplify

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Dashboard from './Dashboard'
 import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifyAuthenticator,  AmplifyAuthContainer } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import Signup from './Signup'
+// import Signup from './Signup'
+
 Amplify.configure(awsconfig);
 
 Auth.configure(awsconfig);
@@ -14,35 +15,36 @@ Auth.configure(awsconfig);
 
 
 function App() {
-  const [authState, setAuthState] = React.useState();
-  const [user, setUser] = React.useState();
+  // const [authState, setAuthState] = React.useState();
+  // const [user, setUser] = React.useState();
 
-  React.useEffect(() => {
-    return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState);
-      console.log("authdata ==>", authData)
-      setUser(authData)
-    });
-  }, []);
+  // React.useEffect(() => {
+  //   return onAuthUIStateChange((nextAuthState, authData) => {
+  //     setAuthState(nextAuthState);
+  //     console.log("authdata ==>", authData)
+  //     setUser(authData)
+  //   });
+  // }, []);
 
-  return authState === AuthState.SignedIn && user ? (
+  // return authState === AuthState.SignedIn && user ? (
 
-    <div className="App">
-      <header className="App-header">
-        <div className="dashboard-wrapper">
-          <Dashboard />
+return(
+  <Authenticator signUpAttributes={[]}>
+    {({ signOut, user }) => (
+      <main>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+        <div className="App">
+          <header className="App-header">
+            <div className="dashboard-wrapper">
+              <Dashboard />
+            </div>
+          </header>
         </div>
-      </header>
-    </div>
-  ): (
-    <AmplifyAuthContainer>
-         <AmplifyAuthenticator>
-         <Signup></Signup>
-    </AmplifyAuthenticator>
-  );
-
-    </AmplifyAuthContainer>
-  );
+      </main>
+    )}
+  </Authenticator>
+)
 }
 
 export default App;
