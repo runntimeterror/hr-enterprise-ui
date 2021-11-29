596c12566d66a6ea8cd4ac1fc2e8974f73306249
Soham Bhattacharjee <soham.onlyn@gmail.com>
2021-11-28 16:39:47 -0800

    historical data

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,25 @@
 export const getFormattedCurrency = (value) => {
-  const currencyFormat =  new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+  const currencyFormat = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
   return currencyFormat.format(value)
+}
+
+export const getLocalDate = (dateString) => {
+  var m = new Date(dateString);
+  return m.getUTCFullYear() + "/" + (m.getUTCMonth() + 1) + "/" + m.getUTCDate();
+}
+
+export const extractArrayFromResponse = (objectArray) => {
+  return objectArray.map((entry) => {
+    const resp = []
+    for (const prop in entry) {
+      if (prop.includes("date")) {
+        resp.push(getLocalDate(entry[prop]))
+      } else if (prop.includes("salary")) {
+        resp.push(getFormattedCurrency(entry[prop]))
+      } else {
+        resp.push(entry[prop])
+      }
+    }
+    return resp
+  })
 }
