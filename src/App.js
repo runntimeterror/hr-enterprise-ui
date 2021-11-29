325b96526152d8d9aaf75503a35bec04851ae74f
Rajat Banerjee <rajatmig29@gmail.com>
2021-11-28 21:16:44 -0800

    removing auth adding facebook auth

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React from 'react';
 import Basic from './Layout/Basic'
 import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import config from './aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
-Amplify.configure(awsconfig);
-Auth.configure(awsconfig);
+var urlsIn = config.oauth.redirectSignIn.split(",");
+var urlsOut = config.oauth.redirectSignOut.split(",");
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType
+};
+var hasLocalhost  = (hostname) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
+var hasHostname   = (hostname) => Boolean(hostname.includes(window.location.hostname));
+var isLocalhost   = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach((e) =>   { if (hasLocalhost(e)) { oauth.redirectSignIn = e; }});
+  urlsOut.forEach((e) =>  { if (hasLocalhost(e)) { oauth.redirectSignOut = e; }});
+}
+else {
+  urlsIn.forEach((e) =>   { if (hasHostname(e)) { oauth.redirectSignIn = e; }});
+  urlsOut.forEach((e) =>  { if (hasHostname(e)) { oauth.redirectSignOut = e; }});
+}
+var configUpdate = config;
+configUpdate.oauth = oauth;
+Amplify.configure(configUpdate);
+Auth.configure(configUpdate);
 
 function App() {
   return (
